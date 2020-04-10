0771522c0da9d266c927e96e3ea1a0c9379773fc
chryswoods <chryswoods@gmail.com>
2020-04-10 13:43:34 +0100

    Added in the first post

--- conf.py
+++ conf.py
@@ -253,7 +253,19 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'github_button': False,
+    'canonical_url': 'https://chryswoods.github.io/blog',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': None,
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': False,
+    'titles_only': False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
