20ef7ec287cfc1326f91915acb18e9e5508d626b
chryswoods <chryswoods@gmail.com>
2020-04-10 11:45:35 +0100

    css and js not visible, why?

--- conf.py
+++ conf.py
@@ -264,7 +264,7 @@ html_theme_path = [alabaster.get_path()]
 html_title = "chryswoods blog"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = html_title
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
--- index.rst
+++ index.rst
@@ -3,8 +3,11 @@ Here is a list of most recent posts:
 .. postlist:: 5
    :excerpts:
 
+   test_post.rst
+
 .. toctree::
    :hidden:
 
    about.rst
+   test_post.rst
 
